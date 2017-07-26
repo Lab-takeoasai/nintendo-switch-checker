97b0c3d634e74f5422fb5adcd1329b10fb4a5383
Takeo Asai <spitson+github@gmail.com>
2017-07-27 06:59:42 +0900

    Add yodobashi

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const Chromy = require('chromy');
 const opn = require('opn');
 const Nintendo = require('./src/nintendo');
+const YodobashiSplatoon = require('./src/yodobashi/splatoon');
 
 async function main() {
-const chromy = new Chromy();
-const nintendo = new Nintendo(chromy);
-const r = await nintendo.fetchStatus();
-console.log("return: ", r)
-chromy.close();
+    const chromy = new Chromy();
+    const client = new YodobashiSplatoon(chromy);
+    const r = await client.fetchStatus();
+    console.log("return: ", r)
+    chromy.close();
 }
 main();
---
+++ src/yodobashi/splatoon.js
@@ -0,0 +1,27 @@
+module.exports = class YodobashiSplatoon {
+
+    constructor(client) {
+        this.client = client;
+    }
+
+    get url() {
+        return 'http://www.yodobashi.com/product/100000001003570628/';
+    }
+
+    async fetchStatus() {
+        const isSoldOut = await this.client.chain().start()
+        .goto(this.url)
+        .evaluate(() => {
+            const stock = document.querySelector('div.salesInfo');
+            if (stock == null) {
+                throw "Too much traffic";
+            }
+            return stock.innerText === '予定数の販売を終了しました';
+        })
+        .end()
+        .catch(_ => {
+            return false; // 混雑エラーページは未だ在庫ありと扱う
+        });
+        return !isSoldOut;
+    }
+}
